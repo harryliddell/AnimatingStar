cd693c8b8d195c8decb67ab3310a063e992b2420
harryliddell <harry.liddell_123@yahoo.co.uk>
2025-11-18 18:45:18 +0000

    Initial Commit

---
+++ AnimatingStar/AnimatingStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingStarApp.swift
+//  AnimatingStar
+//
+//  Created by Harry Liddell on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
